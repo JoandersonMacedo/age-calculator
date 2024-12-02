1ee5f698434cc6da7665598aabd0aa1a1160b8b4
Joanderson Coelho Macedo <joanderson.k10@gmail.com>
2024-12-02 19:03:24 -0300

    Remove node modules

--- age-calculator-app-main/scripts/index.js
+++ age-calculator-app-main/scripts/index.js
@@ -1,28 +1,57 @@
 const form = document.getElementById('age-calculator');
 const inputs = form.querySelector('#inputs');
+const messageInputs = inputs.querySelector('.message-invalid-value')
 const arayImputContainers = inputs.querySelectorAll('.input-container');
 
 arayImputContainers.forEach((container) => {
-    const input = container.querySelector('input');
-    const mensage = container.querySelector('.mensage-invalid-value')
+    const messageInput = container.querySelector('.message-invalid-value')
 
-    input.addEventListener('input',
+    container.addEventListener('input',
         (event) => {
-            const value = parseInt(input.value);
-            const maxValue = parseInt(input.getAttribute('max'));
-            const minValue = parseInt(input.getAttribute('min'));
+            const eventElement = event.target
+            const value = parseInt(eventElement.value);
+            const maxValue = parseInt(eventElement.getAttribute('max'));
+            const minValue = parseInt(eventElement.getAttribute('min'));
 
             if (value > maxValue || value < minValue) {
                 container.classList.add('invalid-value');
-                mensage.textContent = `Must be a valid ${input.getAttribute('name')}`;
-                container.appendChild(mensage)
+                messageInput.textContent = `Must be a valid ${eventElement.getAttribute('name')}`;
             } else {
-                container.classList.remove('invalid')
+                container.classList.remove('invalid-value')
             }
+
+            const nodeArray = Array.from(arayImputContainers);
+            const formValues = nodeArray.map((inputContainer, acumulator = {}) => {
+                const input = inputContainer.querySelector('input');
+                const value = parseInt(input.value);
+                const chave = input.getAttribute('name')
+                return (
+                    {
+                        ...acumulator,
+                        [chave]: value,
+                    }
+                );
+            })
+
+            console.log(formValues)
         }
     )
 });
 
+// inputs.addEventListener('input', (event) => {
+//     const container = event.target.parentElement
+//     const value = parseInt(event.target.value);
+//     const maxValue = parseInt(event.target.getAttribute('max'));
+//     const minValue = parseInt(event.target.getAttribute('min'));
+
+//     if (value > maxValue || value < minValue) {
+//         container.classList.add('invalid-value');
+//         mensage.textContent = `Must be a valid ${input.getAttribute('name')}`;
+//         container.appendChild(mensage)
+//     } else {
+//         container.classList.remove('invalid-value')
+//     }
+// });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
