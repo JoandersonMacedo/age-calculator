3ac142dc26a2e8b2cef6b4af08f4adeb1eed033b
Joanderson Coelho Macedo <joanderson.k10@gmail.com>
2024-12-03 18:27:16 -0300

    Add date-fns

--- age-calculator-app-main/scripts/index.js
+++ age-calculator-app-main/scripts/index.js
@@ -1,3 +1,7 @@
+import { lastDayOfMonth } from 'date-fns';
+import { parse } from 'date-fns/fp';
+
+
 const form = document.getElementById('age-calculator');
 const inputs = form.querySelector('#inputs');
 const messageInputs = inputs.querySelector('.message-invalid-value')
@@ -5,35 +9,45 @@ const arayImputContainers = inputs.querySelectorAll('.input-container');
 
 arayImputContainers.forEach((container) => {
     const messageInput = container.querySelector('.message-invalid-value')
-
+    
     container.addEventListener('input',
         (event) => {
             const eventElement = event.target
             const value = parseInt(eventElement.value);
             const maxValue = parseInt(eventElement.getAttribute('max'));
             const minValue = parseInt(eventElement.getAttribute('min'));
-
+            
             if (value > maxValue || value < minValue) {
                 container.classList.add('invalid-value');
                 messageInput.textContent = `Must be a valid ${eventElement.getAttribute('name')}`;
             } else {
                 container.classList.remove('invalid-value')
             }
-
+            
             const nodeArray = Array.from(arayImputContainers);
-            const formValues = nodeArray.map((inputContainer, acumulator = {}) => {
+            const formValues = {};
+            nodeArray.forEach((inputContainer) => {
                 const input = inputContainer.querySelector('input');
                 const value = parseInt(input.value);
                 const chave = input.getAttribute('name')
-                return (
-                    {
-                        ...acumulator,
-                        [chave]: value,
-                    }
-                );
+                formValues[[chave]]  = value;
             })
-
+            // const formValues = nodeArray.map((inputContainer, acumulator = {}) => {
+            //     const input = inputContainer.querySelector('input');
+            //     const value = parseInt(input.value);
+            //     const chave = input.getAttribute('name')
+            //     return (
+            //         {
+            //             ...acumulator,
+            //             [chave]: value,
+            //         }
+            //     );
+            // })
+            
             console.log(formValues)
+            console.log(`${formValues.year}.${formValues.month}.${formValues.day}`)
+            console.log(parse(`${formValues.year}.${formValues.month}.${formValues.day}`, 'yyyy.MM.dd', new Date()));
+
         }
     )
 });
--- age-calculator-app-main/utilities/ageCalculator.js
+++ age-calculator-app-main/utilities/ageCalculator.js
@@ -1,27 +1,3 @@
-import moment  from "moment";
-
- export function ageCalculatorBeta (date) {
-    const now = moment();
-    let days = date.diff(now, 'days');
-
-    if (days > 30) {
-        days -= months * 30.39;
-        var months = date.subtract(date, 'days').diff(now.subtract(days, 'days'), 'months');
-    } 
-    if (months > 12) {
-        months -= years * 365.24;
-        var years = date.subtract(months, months).diff(now.subtract(days, 'days').subtract(months, 'months'), 'years')
-    }
-
-    return (
-        {
-            years: years.toFixed(0),
-            months: months.toFixed(0),
-            days: days.toFixed(0)
-        }
-    )
-}
-
 export function ageCalculatorAlpha(date) {
     const nowTime = new Date().getTime;
 
