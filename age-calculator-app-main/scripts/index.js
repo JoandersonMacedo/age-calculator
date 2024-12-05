4c2a562195913012d267a4d9e0793da95dd0fd5f
Joanderson Coelho Macedo <joanderson.k10@gmail.com>
2024-12-05 16:47:39 -0300

    age object

--- age-calculator-app-main/scripts/index.js
+++ age-calculator-app-main/scripts/index.js
@@ -1,73 +1,99 @@
-import { lastDayOfMonth } from 'date-fns';
-import { parse } from 'date-fns/fp';
-
+import moment, { months, now } from "moment";
 
 const form = document.getElementById('age-calculator');
 const inputs = form.querySelector('#inputs');
-const messageInputs = inputs.querySelector('.message-invalid-value')
 const arayImputContainers = inputs.querySelectorAll('.input-container');
+const outputDay = document.getElementById('output-day');
+const outputMonth = document.getElementById('output-month');
+const outputYear = document.getElementById('output-year');
 
 arayImputContainers.forEach((container) => {
     const messageInput = container.querySelector('.message-invalid-value')
-    
+
     container.addEventListener('input',
         (event) => {
             const eventElement = event.target
             const value = parseInt(eventElement.value);
             const maxValue = parseInt(eventElement.getAttribute('max'));
             const minValue = parseInt(eventElement.getAttribute('min'));
-            
+
             if (value > maxValue || value < minValue) {
+                inputs.classList.remove('invalid-date');
                 container.classList.add('invalid-value');
                 messageInput.textContent = `Must be a valid ${eventElement.getAttribute('name')}`;
             } else {
                 container.classList.remove('invalid-value')
             }
-            
+
             const nodeArray = Array.from(arayImputContainers);
             const formValues = {};
             nodeArray.forEach((inputContainer) => {
                 const input = inputContainer.querySelector('input');
                 const value = parseInt(input.value);
                 const chave = input.getAttribute('name')
-                formValues[[chave]]  = value;
+                formValues[[chave]] = value;
             })
-            // const formValues = nodeArray.map((inputContainer, acumulator = {}) => {
-            //     const input = inputContainer.querySelector('input');
-            //     const value = parseInt(input.value);
-            //     const chave = input.getAttribute('name')
-            //     return (
-            //         {
-            //             ...acumulator,
-            //             [chave]: value,
-            //         }
-            //     );
-            // })
-            
-            console.log(formValues)
-            console.log(`${formValues.year}.${formValues.month}.${formValues.day}`)
-            console.log(parse(`${formValues.year}.${formValues.month}.${formValues.day}`, 'yyyy.MM.dd', new Date()));
 
+            if (formValues.day && formValues.month && formValues.year && !(moment(`${formValues.day}-${formValues.month}-${formValues.year}`, 'D-M-YYYY').isValid())) {
+                inputs.classList.add('invalid-date');
+            } else {
+                inputs.classList.remove('invalid-date');
+            }
         }
-    )
+    );
 });
-
-// inputs.addEventListener('input', (event) => {
-//     const container = event.target.parentElement
-//     const value = parseInt(event.target.value);
-//     const maxValue = parseInt(event.target.getAttribute('max'));
-//     const minValue = parseInt(event.target.getAttribute('min'));
-
-//     if (value > maxValue || value < minValue) {
-//         container.classList.add('invalid-value');
-//         mensage.textContent = `Must be a valid ${input.getAttribute('name')}`;
-//         container.appendChild(mensage)
-//     } else {
-//         container.classList.remove('invalid-value')
-//     }
-// });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    const formValues = {};
+    arayImputContainers.forEach((container) => {
+        const input = container.querySelector('input');
+        const messageInput = container.querySelector('.message-invalid-value');
+
+        if (input.value === '') {
+            container.classList.add('invalid-value');
+            messageInput.textContent = 'This field is required'
+        } else {
+            container.classList.remove('invalid-value');
+        }
+
+        const value = parseInt(input.value);
+        const chave = input.getAttribute('name')
+        formValues[[chave]] = value;
+    })
+
+    if (formValues.day && formValues.month && formValues.year && (moment(`${formValues.day}-${formValues.month}-${formValues.year}`, 'D-M-YYYY').isValid())) {
+        const now = moment()
+        console.log(now);
+        //date2.diff(date1, 'days');
+        // let toNowYears = moment(`${formValues.day}-${formValues.month}-${formValues.year}`, 'D-MM-YYYY').fromNow();
+        // let toNowMonths = moment(`${formValues.day}-${formValues.month}-${formValues.year + toNowYears}`, 'D-MM-YYYY').fromNow();
+        // let toNowDays = moment(`${formValues.day}-${formValues.month + toNowMonths}-${formValues.year + toNowYears}`, 'D-MM-YYYY').fromNow();
+
+        let toNowYears = now.diff(moment(`${formValues.day}-${formValues.month}-${formValues.year}`, 'D-M-YYYY'), 'years');
+        let toNowMonths = now.diff(moment(`${formValues.day}-${formValues.month}-${formValues.year + toNowYears}`, 'D-M-YYYY'), 'months');
+        let toNowDays = now.diff(moment(`${formValues.day}-${formValues.month + toNowMonths}-${formValues.year + toNowYears}`, 'D-M-YYYY'), 'days');
+
+        const age = {
+            years: toNowYears,
+            months: toNowMonths,
+            days: toNowDays,
+        }
+
+        console.log(age)
+        
+
+        // let moreYears = 1;
+        // let moreMonths = 1;
+        // let moreDays = 1;
+
+        // while(moment(`${formValues.day}-${formValues.month}-${formValues.year}`, 'D-MM-YYYY').isBefore(now)) {
+        //     if(moment(`${formValues.day}-${formValues.month}-${formValues.year + moreYears}`, 'D-MM-YYYY').isBefore(now)) {
+        //         age.years ++;
+        //         moreYears ++;
+        //     }
+        // }
+
+    }
 });
